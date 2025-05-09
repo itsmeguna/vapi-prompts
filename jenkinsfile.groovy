d039684c81b5f8cc6af817f24e230cc4fdf26ace
itsmeguna <itsmegunam@gmail.com>
2025-05-09 18:47:02 +0530

    jenkinsfile

---
+++ jenkinsfile.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    environment {
+       environment {
+            VAPI_URL = 'https://api.vapi.ai/v1/prompts'  
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/itsmeguna/vapi-prompts.git'
+            }
+        }
+
+        stage('Deploy Prompts') {
+            steps {
+                withCredentials([string(credentialsId: 'VAPI_API_KEY', variable: 'VAPI_KEY')]) {
+                    script {
+                        def prompts = readFile('vapi-prompts/ai-prompt.json').trim()
+                        sh """
+                            curl -X POST \
+                                 -H "Content-Type: application/json" \
+                                 -H "Authorization: Bearer $VAPI_KEY" \
+                                -d '${prompts}' \
+                                $VAPI_URL
+
+                        """
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo '✅ Deployment successful!'
+        }
+        failure {
+            echo '❌ Deployment failed.'
